cfa9505b3b9b6f5fe3a08a6e6d61f6d6b09aceb6
Erisson <80925196+ErissonB@users.noreply.github.com>
2024-12-31 18:39:59 -0300

    Added controls eventListeners and redraw logic to createCanvas

--- scripts/start.js
+++ scripts/start.js
@@ -1,33 +1,60 @@
 const mainElement = document.querySelector('main');
 const canvaElement = document.querySelector('#canva');
-createCanvas(mainElement,16);
+let customBackgroundColor = "#ffffff";
+let brushColor = "#000000";
 
+createCanvas(canvaElement,16);
 function createCanvas(parentElement,sizeCanva) {
     if (typeof sizeCanva !== 'number') {
         throw new TypeError(`The "${sizeCanva}" is not a number (${typeof sizeCanva})`);
     }
 
-    const rect = parentElement.getBoundingClientRect();
-    const size = Math.min(rect.width,rect.height)+'px';
-    const newTable = document.createElement("div");
-    newTable.classList.add("table");
-    newTable.style.height = size;
-    newTable.style.width = size;
+    parentElement.querySelectorAll(".grid").forEach((e)=>{e.remove()});
+
+    const height = `${parseInt(parentElement.style.height)/sizeCanva}px`;
+    const width = `${parseInt(parentElement.style.width)/sizeCanva}px`;
 
     for (let i = 0; i < sizeCanva*sizeCanva; i++) {
         const element = document.createElement("div");
         element.classList.add("grid");
+        element.style.height = height;
+        element.style.width = width;
+        element.style.backgroundColor = customBackgroundColor;
         element.addEventListener("mouseenter",()=>{
             element.classList.remove("transition");
-            element.style.backgroundColor = "black";
+            element.style.backgroundColor = brushColor;
         });
         element.addEventListener("mouseleave",()=>{
             element.classList.add("transition");
-            element.style.backgroundColor = "red";
+            element.style.backgroundColor = customBackgroundColor;
 
         });
-        newTable.appendChild(element);
+        parentElement.appendChild(element);
     }
     
-    parentElement.appendChild(newTable);
 }
+
+const controlsElement = document.querySelector("#controls");
+
+controlsElement.addEventListener("input",(e)=>{
+    const id = e.target.id;
+    switch (id) {
+        case "durationUntil":
+            document.documentElement.style.setProperty("--duration", `${e.target.value}s`);
+            break;
+        
+        case "size":
+            createCanvas(canvaElement,parseInt(e.target.value));
+            break;
+        case "backgroundColorControl":
+            customBackgroundColor = e.target.value;
+            canvaElement.querySelectorAll(".grid").forEach((e)=>{e.style.backgroundColor = customBackgroundColor})
+            break;
+
+        case "brushControl":
+            brushColor = e.target.value;
+            break;
+        default:
+            break;
+    }
+})
